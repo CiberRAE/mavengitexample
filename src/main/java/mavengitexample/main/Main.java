a9cdfc4a2bb2cc98adf896cc2a632540cd2aed80
unknown <i202115016@cibertec.edu.pe>
2023-05-23 20:32:32 -0500

    cambio 2 main nuevo banco JUECEPE

--- src/main/java/mavengitexample/main/Main.java
+++ src/main/java/mavengitexample/main/Main.java
@@ -9,14 +9,26 @@ public class Main {
 	public static void main(String[] args) {
 		EntityManager em = Conn.getInstancia().getFactory().createEntityManager();
 		
-		Banco bancoSaga = new Banco();
+		/*Banco bancoSaga = new Banco();
 		bancoSaga.setBancoNombre("Saga");
 		
 		em.getTransaction().begin();
 		em.persist(bancoSaga);
 		em.getTransaction().commit();
 		
-		System.out.println("Se creo un nuevo banco");
+		System.out.println("Se creo un nuevo banco");*/
+		
+		Banco bcpBanco = new Banco();
+		bcpBanco.setBancoNombre("BCP");
+		
+		em.getTransaction().begin();
+		em.persist(bcpBanco);
+		em.getTransaction().commit();
+		
+		Banco bcpResultado = em.find(Banco.class, 2);
+		
+		System.out.println("Se creo el JUECPE");
+		em.close();
 	}
 
 }
